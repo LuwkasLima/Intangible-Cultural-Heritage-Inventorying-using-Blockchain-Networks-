4048ddb34ea45c087f341131009b303eac908559
Lucas Lima <lucas.maiasouzalima@kcc.com>
2023-04-01 19:10:54 -0500

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Intangible Cultural Heritage Inventorying with Blockchain Networks</span>,
+  project: {
+    link: 'https://github.com/LuwkasLima/Intangible-Cultural-Heritage-Inventorying-using-Blockchain-Networks-',
+  },
+  chat: {
+    link: 'https://twitter.com',
+  },
+  docsRepositoryBase: 'https://github.com/LuwkasLima/Intangible-Cultural-Heritage-Inventorying-using-Blockchain-Networks-',
+  footer: {
+    text: 'Intangible Cultural Heritage Inventorying using Blockchain Networks © 2023 by Lucas Maia Souza Lima is licensed under CC BY-SA 4.0. ',
+  },
+}
+
+export default config
